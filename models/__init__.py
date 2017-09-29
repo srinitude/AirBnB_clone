1a100c7bbe9b2690f00de0ebf2766a6787855432
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-29 22:54:11 +0000

    created a class FileStorage that serializes instances to JSON file and deserializes JSON file to instances

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+to create a unique FileStorage instance for your application
+"""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Serializes instances to a JSON file and deserializes
+JSON file to instances
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes
+    JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        returns the dictionary
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes __objects to JSON file
+        """
+        new_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            obj_dict = obj.to_dict()
+            new_dict[key] = obj_dict
+        with open(FileStorage.__file_path, "w") as fs:
+            json.dump(new_dict, fs)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        """
+        try:
+            with open(FileStorage.__file_path, "r") as fs:
+                obj_dicts = json.load(fs)
+                for key, d in obj_dicts.items():
+                    new_obj = BaseModel(**d)
+                    FileStorage.__objects[key] = new_obj
+        except FileNotFoundError:
+            return
