3221ed110bdd7f17a79dd080ec3abbdda517acd9
Kiren Srinivasan <srinitude@gmail.com>
2017-09-29 19:49:26 +0000

    Adds updated base model with ISO string format

--- models/base_model.py
+++ models/base_model.py
@@ -15,18 +15,18 @@ class BaseModel:
         """
         Initializer of a BaseModel
         """
-        date_format = "%Y-%m-%d %H:%M:%S.%f"
+        fmt = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             if "id" in kwargs:
                 self.id = kwargs["id"]
             if "created_at" in kwargs:
-                self.created_at = datetime.strptime(kwargs["created_at"], date_format)
+                self.created_at = datetime.strptime(kwargs["created_at"], fmt)
             if "updated_at" in kwargs:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], date_format)
+                self.updated_at = datetime.strptime(kwargs["updated_at"], fmt)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -49,6 +49,6 @@ class BaseModel:
         """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = str(new_dict["created_at"])
-        new_dict["updated_at"] = str(new_dict["updated_at"])
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
