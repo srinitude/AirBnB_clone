d667f5cae1e798c67846b12ddb84867087197286
Kiren Srinivasan <srinitude@gmail.com>
2017-09-30 19:55:53 +0000

    Adds objects to file storage even when dict is used to initialize

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def __str__(self):
         """
